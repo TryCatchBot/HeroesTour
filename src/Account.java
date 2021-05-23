fc1f968800a0da3ad7b5259feb40cbdf9f99f271
pastorcoder <zuma4good@gmail.com>
2021-05-23 12:33:46 +0100

    new file structure

--- src/Account.java
+++
@@ -1,9 +0,0 @@
-// Fig. 3.1: Account.java
-// Account class that contains a name instance variable
-// and methods to set and get its value.
-
-
-public class Account {
-
-
-}
---
+++ src/chapter3/Account.java
@@ -0,0 +1,9 @@
+package chapter3;// Fig. 3.1: chapter3.Account.java
+// chapter3.Account class that contains a name instance variable
+// and methods to set and get its value.
+
+
+public class Account {
+
+
+}
